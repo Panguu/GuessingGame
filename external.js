5884e5e45579e8866e9d8e45e14ff031ce748e7a
Franciely Beckert <francielybeckert@gmail.com>
2025-01-27 11:19:41 +0100

    Add getPlayerGuess and checkGuess functions

--- external.js
+++ external.js
@@ -1,3 +1,36 @@
 function generateRandomNumber() {
-    return Math.floor((Math.random() * 100) + 1);
+  return Math.floor(Math.random() * 100 + 1);
 }
+
+const getPlayerGuess = function () {
+  let guess;
+  while (true) {
+    guess = prompt(
+      "Welcome Player 😎! Please enter an integer between 1 and 100:"
+    );
+    const numberGuess = Number(guess);
+
+    if (
+      !Number.isNaN(numberGuess) &&
+      Number.isInteger(numberGuess) &&
+      numberGuess >= 1 &&
+      numberGuess <= 100
+    ) {
+      return numberGuess;
+    } else {
+      alert(
+        "Wrong input 😵! Please enter a integer between 1 and 100. No '.' or ',' will be accepted"
+      );
+    }
+  }
+};
+
+const checkGuess = function (playerGuess, correctNumber) {
+  if (playerGuess > correctNumber) {
+    return "Too high!😝";
+  }
+  if (playerGuess < correctNumber) {
+    return "Too low! 😜";
+  }
+  return "Correct! 🎉";
+};
